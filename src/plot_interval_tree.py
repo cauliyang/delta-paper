6844372f9b50acd128fc20d767d1a95ac6317595
yangli <li002252@umn.edu>
2022-09-19 22:25:13 -0500

    add abstract

--- src/plot_interval_tree.py
+++ src/plot_interval_tree.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 @author: YangyangLi
-@contact:li002252@umn.edu
+@contact: yangyang.li@northwestern.edu
 """
 
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
